6163dd9711f6a376e4ec39d5009165f0a7f667bd
Nikhil Saxena <91839763+Cleveridiot07@users.noreply.github.com>
2025-03-16 10:19:38 +0530

    created vercel.json

--- frontend/src/components/AddNote/CloseAddNote.jsx
+++ frontend/src/components/AddNote/CloseAddNote.jsx
@@ -9,7 +9,7 @@ const CloseAddNote = (props) => {
   return (
     <div
       onClick={handleClick} // Use handleClick function to handle onClick event
-      className="fixed top-10 right-20 z-30 mx-auto w-[80px] h-[80px] border z-50 border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:rounded-3xl flex items-center justify-center cursor-pointer ">
+      className="fixed top-10 right-20  mx-auto w-[80px] h-[80px] border z-50 border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:rounded-3xl flex items-center justify-center cursor-pointer ">
       <span className='text-6xl text-center text-rose-500'>&#215;</span>
     </div>
   );
